b63ebbf0bfb7256d6cd6e66f23e51b58d78561dd
Ksenia Klamut <ksenia.klamut@gmail.com>
2019-04-09 23:25:36 +0200

    new stuff

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const path = require('path');
 const app = express();
 const port = 4001;
 
+const upload = require('express-fileupload');
+app.use(upload());
+
 app.use(express.static('static'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/main.html')));
+app.get('/upload', (req, res) => res.sendFile(path.join(__dirname + '/upload.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ static/js/first.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    const plus = document.getElementById('plus');
+
+    plus.addEventListener('click', function () {
+        window.location.href = "/upload";
+    });
+};
