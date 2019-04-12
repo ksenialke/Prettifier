10e65bcb01fc6c291081e067b1ef9458f71c1ba6
Ksenia Klamut <ksenia.klamut@gmail.com>
2019-04-13 00:20:24 +0200

    modifying uploading page

--- static/js/first.js
+++ static/js/first.js
@@ -1,16 +1,27 @@
 const input = document.querySelector('input[type="file"]');
+const photoArea = document.querySelector('.photo');
+var label = document.getElementById('labelfile');
+let fileIsUploaded = false;
+
+if (!fileIsUploaded) {
+    // [0] because these are node lists
+    document.getElementsByClassName('edits')[0].style.display = 'none';
+    document.getElementsByClassName('filters')[0].style.display = 'none';
+    document.getElementsByClassName('title')[0].style.height = '10vh';
+    document.getElementById('choose-file').style.height = '20vh';
+    document.getElementsByClassName('photo')[0].style.height = '70vh';
+}
 
 
-input.addEventListener('change', function (e) {
-    console.log(input.files);
+function handleFiles(files) {
     const reader = new FileReader();
     reader.onload = function () {
         const img = new Image();
         img.onload = function () {
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
-            canvas.width = img.width;
-            canvas.height = img.height;
+            canvas.width = mapCanvasWidth(canvas, photoArea);
+            canvas.height = mapCanvasHeight(canvas, photoArea);
             context.drawImage(img, 0, 0);
 
             const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
@@ -21,11 +32,79 @@ input.addEventListener('change', function (e) {
                 data[i + 1] = avg;
                 data[i + 2] = avg;
             }
+
             context.putImageData(imageData, 0, 0);
-            document.body.appendChild(canvas);
+
+            photoArea.appendChild(canvas);
+            canvas.toDataURL();
         };
-        img.src = reader.result;
-        // document.body.appendChild(img);
+        img.src = reader.result
     };
-    reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(files[0]);
+}
+
+function mapCanvasWidth(canvas, div) {
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+    let divWidth = div.scrollWidth;
+    let divHeight = div.scrollHeight;
+    let ratio = canvasHeight / canvasWidth;
+
+    if (ratio >= 1) {
+        // canvasHeight = divHeight;
+        canvasWidth = Math.floor(divHeight / ratio);
+    }
+    else {
+        canvasWidth = divWidth;
+        // canvasHeight = Math.floor(divWidth * ratio);
+    }
+    return canvasWidth;
+}
+
+function mapCanvasHeight(canvas, div) {
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+    let divWidth = div.scrollWidth;
+    let divHeight = div.scrollHeight;
+    let ratio = canvasHeight / canvasWidth;
+
+    if (ratio >= 1) {
+        canvasHeight = divHeight;
+        // canvasWidth = Math.floor(divHeight / ratio);
+    }
+    else {
+        // canvasWidth = divWidth;
+        canvasHeight = Math.floor(divWidth * ratio);
+    }
+    return canvasHeight;
+}
+
+input.addEventListener('change', function (e) {
+    handleFiles(input.files)
 }, false);
+
+// Drag and drop feature
+document.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    e.stopPropagation()
+}, false);
+document.addEventListener('drop', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    handleFiles(e.dataTransfer.files)
+}, false);
+
+// Make 'upload file' label disappear and appear filters and edits
+label.addEventListener('click', function () {
+    document.getElementsByClassName('edits')[0].style.display = 'block';
+    document.getElementsByClassName('edits')[0].style.height = '20vh';
+    document.getElementsByClassName('filters')[0].style.display = 'block';
+    document.getElementsByClassName('filters')[0].style.height = '25vh';
+    document.getElementsByClassName('photo')[0].style.height = '45vh';
+
+    setTimeout(function () {
+        document.getElementById('choose-file').style.display = 'none';
+    }, 100);
+    fileIsUploaded = true;
+});
+
