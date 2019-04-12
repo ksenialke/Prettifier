bbcad04f923019107264571af3574a71bfeaefee
Ksenia Klamut <ksenia.klamut@gmail.com>
2019-04-12 18:26:10 +0200

    added canvas

--- static/js/first.js
+++ static/js/first.js
@@ -1,7 +1,29 @@
-window.onload = function () {
-    const plus = document.getElementById('plus');
+const input = document.querySelector('input[type="file"]');
 
-    plus.addEventListener('click', function () {
-        window.location.href = "/upload";
-    });
-};
+
+input.addEventListener('change', function (e) {
+    console.log(input.files);
+    const reader = new FileReader();
+    reader.onload = function () {
+        const img = new Image();
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0);
+
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            for (var i = 0; i <= data.length; i += 4) {
+                const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                data[i] = avg;
+                data[i + 1] = avg;
+                data[i + 2] = avg;
+            }
+            context.putImageData(imageData, 0, 0);
+            document.body.appendChild(canvas);
+        };
+        img.src = reader.result;
+        // document.body.appendChild(img);
+    };
+    reader.readAsDataURL(input.files[0]);
+}, false);
